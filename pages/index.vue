25130b38d806405f3bfb369043abadf12dfbfb07
Paul Anthony McGowan <xhostcom@gmail.com>
2020-04-05 21:54:04 +0100

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="container">
+ <section class="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
   <header class="bg-white black-80 tc pv4 avenir">
   <a href="/" class="bg-black-80 ba b--black dib pa3 w2 h2 br-100">
   <svg class="white" data-icon="heart" viewBox="0 0 32 32" style="fill:currentcolor"><title>heart icon</title><path d="M16 0 C6 0 2 4 2 14 L2 22 L6 24 L6 30 L26 30 L26 24 L30 22 L30 14 C30 4 26 0 16 0 M9 12 A4.5 4.5 0 0 1 9 21 A4.5 4.5 0 0 1 9 12 M23 12 A4.5 4.5 0 0 1 23 21 A4.5 4.5 0 0 1 23 12"></path></svg>
   </a>
-  <h1 class="mt2 mb0 baskerville i fw1 f1">Valentine Quotes</h1>
+  <h1 class="mt2 mb0 baskerville i fw1 f1">Spoil Your Loved One With a Random Quote!</h1>
   </header>
-
-      <article class="mw5 mw6-ns center pt4">
+<article class="mw5 mw6-ns center pt4">
   <div class="aspect-ratio aspect-ratio--1x1 mb4">
     <div class="aspect-ratio--object cover" style="background:url(heart.webp) center;"></div>
   </div>
   <a class="f6 link dim br3 ba bw2 ph3 pv2 mt2 dib dark-pink" href="#0">Get A Valentines Quote</a>
 </article>
-
-  </div>
-
+</section>
 </template>
 <script>
 export default {}
